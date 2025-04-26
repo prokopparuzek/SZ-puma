c5629c2e0a68c850b85ef36c661a42a13f920ea1
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2025-04-26 22:59:28 +0200

    add pause

--- music/morse.h
+++ music/morse.h
@@ -6,29 +6,47 @@
 // fedora
 const uint16_t morse1[] PROGMEM = {
     a2, CTVRT / 2, a2, CTVRT / 2, a2, CTVRT, a2, CTVRT / 2,  // F
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT / 2,                                           // E
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT, a2, CTVRT / 2, a2, CTVRT / 2,                 // D
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT, a2, CTVRT, a2, CTVRT,                         // O
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT / 2, a2, CTVRT, a2, CTVRT / 2,                 // R
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT / 2, a2, CTVRT,                                // A
+    POMLKA, CTVRT,                                           //
 };
 
 // ubuntu
 const uint16_t morse2[] PROGMEM = {
     a2, CTVRT / 2, a2, CTVRT / 2, a2, CTVRT,                 // U
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT, a2, CTVRT / 2, a2, CTVRT / 2, a2, CTVRT / 2,  // B
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT / 2, a2, CTVRT / 2, a2, CTVRT,                 // U
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT, a2, CTVRT / 2,                                // N
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT,                                               // T
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT / 2, a2, CTVRT / 2, a2, CTVRT,                 // U
+    POMLKA, CTVRT,                                           //
 };
 
 // debian
 const uint16_t morse3[] PROGMEM = {
     a2, CTVRT, a2, CTVRT / 2, a2, CTVRT / 2,                 // D
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT / 2,                                           // E
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT, a2, CTVRT / 2, a2, CTVRT / 2, a2, CTVRT / 2,  // B
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT / 2, a2, CTVRT / 2,                            // I
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT / 2, a2, CTVRT,                                // A
+    POMLKA, CTVRT / 2,                                       //
     a2, CTVRT, a2, CTVRT / 2,                                // N
+    POMLKA, CTVRT,                                           //
 };
